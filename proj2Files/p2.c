02d5fa00377f8aae3c328ca8ee130b66352bde02
loki <isaacgcallison@gmail.com>
2019-02-13 22:27:45 -0600

    ok

--- proj2Files/p2.c
+++ proj2Files/p2.c
@@ -1,39 +1,47 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #define PAGESIZE getpagesize()
 #define ALIGNMENT 16
-#define GET(p)       (*(unsigned int *)(p))
-#define PUT(p, val) (*(unsigned int *)(p) = (val))
-#define PACK(size, alloc) ((size) | (alloc))
-#define GET_SIZE(p)  (GET(p) & ~0x7)
-#define GET_ALLOC(p) (GET(p) & 0x1)
+#define GET(p) (*(unsigned int *)(p))
+#define END -1
+//#define struct node * node_ptr
 void * region;
-int * heapStart;
-int * end;
-int * p;
-int len;
-int * next;
+int * head;
+static unsigned int numByteCount;
+bool wholeBlockalloc;
+
+typedef struct node {
+  bool alloc;
+  unsigned int size;
+  struct node * this;
+  struct node * next;
+} node;
+struct node * p;
 
 //will mask out last bit to zero ->>>>  int y = (x & -2);
 void heap_init(int num_pages_for_heap)
 {
+    // allocate bytes requried by init.
     int numBytesToAllocate = (num_pages_for_heap * PAGESIZE);
+    // set counter //
+    numByteCount = numBytesToAllocate;
     region = mmap(NULL, (numBytesToAllocate), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0 );
-    heapStart = region;
-    end = (heapStart + numBytesToAllocate);
-
-    // Initialize memory region to zero
-    for(int i = 0; i < numBytesToAllocate; i++){ *(heapStart+i) & 0; }
-
-    // Initialize whole memory with size and unallocated:
-    PUT(heapStart, PACK(numBytesToAllocate, 0));
-
-    printf("WORKING CODE/heap_init: heapStart: %p + numBytesToAllocate: %d = end: %p\n", heapStart, numBytesToAllocate, end );
-    printf("\n" );
+    // set head pointer //
+    head = region;
+    // set flag //
+    wholeBlockalloc = false;
+    node head;
+    head.size = numBytesToAllocate;
+    head.this = region;
+    head.next = NULL;
+    head.alloc = false;
+    printf("head size %d\n",head.size );
+    p = head.this;
 
 }
 
@@ -41,49 +49,31 @@ void heap_init(int num_pages_for_heap)
 // if num_bytes_to_allocate = zero return null/ if unsuccessful returns null and sets errno
 void *heap_alloc(int num_bytes_to_allocate)
 {
-      // pads the data requested in case it is not a multiple of 16
-    if(num_bytes_to_allocate % ALIGNMENT != 0){
-        len += num_bytes_to_allocate%16;
-      }
-
-    p = heapStart;
-
-    printf("GET_SIZE p: %d\n",GET_SIZE(p) );
-    printf("GET_ALLOC p: %d\n",GET_ALLOC(p) );
-
-    //First FIt algorithm???
-    while( (p < end)
-            &&  ((*p & 1)
-            ||  (*p <= len)) )
-    {
-      p = p + (*p & -2);
-      //printf("P at time 50-> %p :  %d\n",p, *p );
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // set flag if whole block is allocated //
+  if(num_bytes_to_allocate > numByteCount){wholeBlockalloc == true;}
+  // if current block is not allocated and the the bloc still has bytes to give up //
+  if( (p->alloc == false) && (numByteCount > num_bytes_to_allocate)){
+    // set size to num bytes needed and flip allocated bit, set next to null to tack on another node;
+    p->size = num_bytes_to_allocate;
+    p->alloc = true;
+    p->next = NULL;
+    // decrement
+    numByteCount = (numByteCount - num_bytes_to_allocate);
+  } else {
+    // traverse nodes
+    while(p->next != NULL){
+      p = (p->next);
     }
-    printf("Heapstart:%p : heapend: %p\n",heapStart, end );
-    return p;
+    if(p->next == NULL && wholeBlockalloc != true){
+
+    }
+  }
+//////////////////////////////////////////////////////////////////////////////////
 }
 
 // returns bl;ock to pool of free memory
 void heap_free(void *pointer_to_area_to_free)
 {
     return;
-}
-
-
-// allocate a free block if needs are less than required
-void addBlock(int * p, int len){
-  int newsize = ((len + 1) >> 1) << 1; // round up to even
-  int oldsize = (*p & -2);// mask out low bit
-  *p = newsize | 1; // set new length
-  if (newsize < oldsize){ // set length in remaining part of the block
-    *(p + newsize) = oldsize - newsize;
-  }
-}
-
-void free_block(int * p){
-  *p = *p & -2; // clear allocated flag
-  next = p + *p; // find next block
-  if( (*next & 1) == 0){
-    *p = *p + *next; // add to    this block if not allocated
-  }
 }
